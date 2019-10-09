3d865e426f0a8c7d34e983112bbf3ebaefdd4278
listedforlife <clarian@listedproductions.com>
2019-10-09 10:11:39 -0400

    form handler again

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -52,7 +52,36 @@ export const ContactPageTemplate = ({
         </div>
 
         <div>
-          <FormSimple2 />
+      
+      <form name="listed-contact" 
+      className='Form'
+      action="post" 
+      data-netlify="true"
+      netlify-honeypot="bot-field" 
+      >
+  <p>
+    <label className='Form--Label'>Your Name: 
+    <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label className='Form--Label'>Your Email: 
+    <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label className='Form--Label'>Your Role: 
+    <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label className='Form--Label'
+    >Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
         </div>
       </div>
     </section>
