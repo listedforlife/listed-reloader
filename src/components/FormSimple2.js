3b0a5035b5ed66acecda5d5795646f555910b375
listedforlife <clarian@listedproductions.com>
2019-10-09 10:01:42 -0400

    form resubmit

--- src/components/FormSimple2.js
+++ src/components/FormSimple2.js
@@ -8,19 +8,21 @@ const FormSimple2 = () => {
 
 <form name="listed-contact" 
       className='Form'
-      method="post" 
+      action="post" 
       data-netlify="true"
-      netlify 
       netlify-honeypot="bot-field" 
-      hidden>
+      >
   <p>
-    <label className='Form--Label'>Your Name: <input type="text" name="name" /></label>   
+    <label className='Form--Label'>Your Name: 
+    <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label className='Form--Label'>Your Email: <input type="email" name="email" /></label>
+    <label className='Form--Label'>Your Email: 
+    <input type="email" name="email" /></label>
   </p>
   <p>
-    <label className='Form--Label'>Your Role: <select name="role[]" multiple>
+    <label className='Form--Label'>Your Role: 
+    <select name="role[]" multiple>
       <option value="leader">Leader</option>
       <option value="follower">Follower</option>
     </select></label>
