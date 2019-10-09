d1ad75a10f79bf7c4782464f2832c60aad059bbe
listedforlife <clarian@listedproductions.com>
2019-10-09 10:47:03 -0400

    for honey-pot

--- src/components/FormSimple2.js
+++ src/components/FormSimple2.js
@@ -6,26 +6,36 @@ const FormSimple2 = () => {
 
     return (
 
-<form name="listed" method="post" data-netlify="true" data-netlify-honeypot="bot-field">  <input type="hidden" name="bot-field" />  <input type="hidden" name="form-name" value="listed" />  <div className="field half first">
+<form name="listed" 
+      method="post" 
+      data-netlify="true" 
+      >  
+      
+      <input type="hidden" name="bot-field" />  
+      <input type="hidden" name="form-name" value="listed" />  
+      
+      <div className="field half first">
+    
     <label htmlFor="name">Name</label>
     <input type="text" name="name" id="name" />
-  </div>
-  <div className="field half">
+      </div>
+  
+    <div className="field half">
     <label htmlFor="email">Email</label>
     <input type="text" name="email" id="email" />
-  </div>
-  <div className="field">
+    </div>
+    <div className="field">
     <label htmlFor="message">Message</label>
     <textarea name="message" id="message" rows="6" />
-  </div>
-  <ul className="actions">
-    <li>
+    </div>
+    <ul className="actions">
+      <li>
       <input type="submit" value="Send Message" className="special" />
-    </li>
-    <li>
+      </li>
+      <li>
       <input type="reset" value="Clear" />
     </li>
-  </ul>
+    </ul>
 </form>
 )
 }
