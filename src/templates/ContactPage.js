b2d98485062899b94885ec9057ccdfe7886a4087
listedforlife <clarian@listedproductions.com>
2019-10-09 10:24:10 -0400

    form test

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -52,27 +52,7 @@ export const ContactPageTemplate = ({
         </div>
 
         <div>
-        <form name="contacttest" method="post" data-netlify="true" data-netlify-honeypot="bot-field">  <input type="hidden" name="bot-field" />  <input type="hidden" name="form-name" value="contact" />  <div className="field half first">
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
-  </div>
-  <div className="field half">
-    <label htmlFor="email">Email</label>
-    <input type="text" name="email" id="email" />
-  </div>
-  <div className="field">
-    <label htmlFor="message">Message</label>
-    <textarea name="message" id="message" rows="6" />
-  </div>
-  <ul className="actions">
-    <li>
-      <input type="submit" value="Send Message" className="special" />
-    </li>
-    <li>
-      <input type="reset" value="Clear" />
-    </li>
-  </ul>
-</form>
+        <FormSimple2/>
         </div>
       </div>
     </section>
