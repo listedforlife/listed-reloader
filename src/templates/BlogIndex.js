4c3c441d6bc2795d6f05b7ba685099e4762a64e0
listedforlife <clarian@listedproductions.com>
2019-10-20 18:36:07 -0400

    performance enhancement api

--- src/templates/BlogIndex.js
+++ src/templates/BlogIndex.js
@@ -20,20 +20,6 @@ export const byDate = posts => {
   return posts.filter(post => Date.parse(post.date) <= now)
 }
 
-/**
- * filter posts by category.
- *
- * @param {posts} object
- * @param {title} string
- * @param {contentType} string
- */
-export const byCategory = (posts, title, contentType) => {
-  const isCategory = contentType === 'postCategories'
-  const byCategory = post =>
-    post.categories &&
-    post.categories.filter(cat => cat.category === title).length
-  return isCategory ? posts.filter(byCategory) : posts
-}
 
 // Export Template for use in CMS preview
 export const BlogIndexTemplate = ({
@@ -72,21 +58,22 @@ export const BlogIndexTemplate = ({
       </div>
     </section>
 
+
+{!!postCategories.length && (
+  <section className="section thin">
+    <div style={{textAlign:'center',marginTop:'-95px'}}>
+      <PostCategoriesNav enableSearch categories={postCategories} />
+    </div>
+  </section>
+)}
           {!!posts.length && (
-            <section style={{marginTop:'-35px'}} className="section">
+            <section style={{marginTop:'-35px'}}  className="section">
               <div className="container">
                 <PostSection posts={filteredPosts} />
               </div>
             </section>
           )}
 
-{!!postCategories.length && (
-            <section className="section thin">
-              <div style={{textAlign:'center'}}>
-                <PostCategoriesNav enableSearch categories={postCategories} />
-              </div>
-            </section>
-          )}
         </main>
       )
     }}
