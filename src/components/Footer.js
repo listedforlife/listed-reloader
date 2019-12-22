096f912168a33dd2d4ed6a6022b060fe0d4ba165
01Clarian <clarionnorth@gmail.com>
2019-12-22 15:31:15 -0500

    buzz switch from news and target blank subscribe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ export default () => (
     style={{textAlign:'center'}}>
     <a
     className='example_d'
-    href="https://visitor.r20.constantcontact.com/d.jsp?llr=csi9ozbab&p=oi&m=csi9ozbab&sit=zaxq5c9bb&f=56937576-3074-4bab-becd-a2b8ce970e8b">
+    href="https://visitor.r20.constantcontact.com/d.jsp?llr=csi9ozbab&p=oi&m=csi9ozbab&sit=zaxq5c9bb&f=56937576-3074-4bab-becd-a2b8ce970e8b" target="_blank">
     Subscribe</a>
     </div>
     <br />
