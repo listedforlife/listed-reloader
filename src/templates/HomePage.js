046615f8357e017b039913cf3ff5b83bf6715335
Drew Proud <therealdrewproud@gmail.com>
2020-08-18 11:55:06 -0500

    Set homepage artist links to be generated programmatically

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { Location } from '@reach/router'
 import PostSection from '../components/PostSection'
 import Layout from '../components/Layout'
 import './HomePage.css'
-import ReactMarkdown from 'react-markdown'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({
@@ -15,82 +13,132 @@ export const HomePageTemplate = ({
   listedmix,
   listedmixlk,
   posts = [],
+  artists = []
 }) => (
   <Location>
     {() => {
-    
       return (
         <main className="Blog">
-    <section className="section hide">
-      <div className="container">
-      </div>
-    </section>
-            <section className="section">
-              <div className="container">
-                <PostSection posts={posts} />
-              </div>
-            </section>
+          <section className="section hide">
+            <div className="container"></div>
+          </section>
+          <section className="section">
+            <div className="container">
+              <PostSection posts={posts} />
+            </div>
+          </section>
+          <div>
+            <h1> {listedmix}</h1>
+            <iframe
+              title="listed-playlist"
+              width="100%"
+              height="300"
+              scrolling="no"
+              frameBorder="no"
+              allow="autoplay"
+              src={listedmixlk}
+            ></iframe>
             <div>
-    <h1> {listedmix}</h1>
-    <iframe title="listed-playlist" width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src={listedmixlk}></iframe>
-    <div>
-      <div class="home-artists-links" style={{}}>
-    <h1>Artists</h1>
-    
-   {subT = 
-   <div style={{textAlign:'center'}}>
-   <ReactMarkdown style={{ fontSize: '8px'}} source={subtitle} />     
-   </div>
-}
-   <div className="dropdown">
-		<button className="dropdown-button" style={{visibility:'hidden'}}>Artists</button>
-		<div className="dropdown-list">
-    </div>
-    </div>
-	</div>
-<div className="ctct-inline-form" data-form-id="77fa4a78-f78b-4c9b-9057-b015694f8ed3"></div>
-    </div>
-    </div>
+              <div class="home-artists-links" style={{}}>
+                <h1>Artists</h1>
+
+                {
+                  (subT = (
+                    <div style={{ textAlign: 'center' }}>
+                      <p>
+                        {artists.map(artist => (
+                          <a style={{ marginRight: '1em' }} href={artist.slug}>
+                            {artist.title.trim()}
+                          </a>
+                        ))}
+                      </p>
+                    </div>
+                  ))
+                }
+                <div className="dropdown">
+                  <button
+                    className="dropdown-button"
+                    style={{ visibility: 'hidden' }}
+                  >
+                    Artists
+                  </button>
+                  <div className="dropdown-list"></div>
+                </div>
+              </div>
+              <div
+                className="ctct-inline-form"
+                data-form-id="77fa4a78-f78b-4c9b-9057-b015694f8ed3"
+              ></div>
+            </div>
+          </div>
         </main>
       )
     }}
-    
   </Location>
 )
 
-
 // Export Default BlogIndex for front-end
-const HomePage = ({ data: { page, posts
- } }) => (
+const HomePage = ({ data: { page, posts, artists } }) => (
   <Layout
     meta={page.frontmatter.meta || false}
-    title={page.frontmatter.title || false}>
-      <div className='fixcenter'>
-    <div className='slider'>
-  <div className='slide1' style={{background: `url(${page.frontmatter.featuredImage})no-repeat center`,backgroundSize:'cover'}}></div>
-  <div className='slide2' style={{background: `url(${page.frontmatter.featuredImage2})no-repeat center`,backgroundSize:'cover'}}></div>
-  <div className='slide3' style={{background: `url(${page.frontmatter.featuredImage3})no-repeat center`,backgroundSize:'cover'}}></div>
-  <div className='slide4' style={{background: `url(${page.frontmatter.featuredImage4})no-repeat center`,backgroundSize:'cover'}}></div>
-  <div className='slide5' style={{background: `url(${page.frontmatter.featuredImage5})no-repeat center`,backgroundSize:'cover'}}></div>
-</div>
-</div>
-<h1>Buzz</h1>
+    title={page.frontmatter.title || false}
+  >
+    <div className="fixcenter">
+      <div className="slider">
+        <div
+          className="slide1"
+          style={{
+            background: `url(${page.frontmatter.featuredImage})no-repeat center`,
+            backgroundSize: 'cover'
+          }}
+        ></div>
+        <div
+          className="slide2"
+          style={{
+            background: `url(${page.frontmatter.featuredImage2})no-repeat center`,
+            backgroundSize: 'cover'
+          }}
+        ></div>
+        <div
+          className="slide3"
+          style={{
+            background: `url(${page.frontmatter.featuredImage3})no-repeat center`,
+            backgroundSize: 'cover'
+          }}
+        ></div>
+        <div
+          className="slide4"
+          style={{
+            background: `url(${page.frontmatter.featuredImage4})no-repeat center`,
+            backgroundSize: 'cover'
+          }}
+        ></div>
+        <div
+          className="slide5"
+          style={{
+            background: `url(${page.frontmatter.featuredImage5})no-repeat center`,
+            backgroundSize: 'cover'
+          }}
+        ></div>
+      </div>
+    </div>
+    <h1>Buzz</h1>
 
     <HomePageTemplate
       {...page}
       {...page.fields}
       {...page.frontmatter}
-      
       posts={posts.edges.map(post => ({
         ...post.node,
         ...post.node.frontmatter,
         ...post.node.fields
       }))}
-    
+      artists={artists.edges.map(artist => ({
+        title: artist.node.frontmatter.title,
+        slug: artist.node.fields.slug
+      }))}
     />
-
   </Layout>
-  
 )
 
 export default HomePage
@@ -101,7 +149,6 @@ export const pageQuery = graphql`
   ## $id is processed via gatsby-node.js
   ## query name must be unique to this file
   query HomePage($id: String!) {
-
     page: markdownRemark(id: { eq: $id }) {
       ...Meta
       html
@@ -119,7 +166,7 @@ export const pageQuery = graphql`
         featuredImage2
         featuredImage3
         featuredImage4
-        featuredImage5       
+        featuredImage5
       }
     }
 
@@ -142,6 +189,21 @@ export const pageQuery = graphql`
         }
       }
     }
+
+    artists: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "artists" } } }
+      sort: { order: ASC, fields: [frontmatter___title] }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
   }
 `
-
